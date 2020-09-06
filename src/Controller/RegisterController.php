95f005f4326dc920eaeb4b02729782591f72f3d5
Jonathan Mangarret <jmangarret@gmail.com>
2020-09-06 13:24:39 -0500

    Registro de usuario con password encriptado e integracion de bootstrap con webpack encore bundle. Ruta register.

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if (($form->isSubmitted()) && ($form->isValid())) {
+            $em = $this->getDoctrine()->getManager();
+            $password = $passwordEncoder->encodePassword($user, $form['password']->getData());
+            $user->setPassword($password);
+            $user->setRoles(["ROLE_USER"]);
+            $em->persist($user);
+            $em->flush();
+            
+            $this->addFlash('success','Registrado con éxito!');
+
+            return $this->redirectToRoute('register');
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
